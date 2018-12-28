d257b78bbb0b93a6f1f52d654b3ae675029185d4
kaustav <kaustavkarmakar2@gmail.com>
2018-12-28 15:40:48 +0530

    first push

---
+++ song.js
@@ -0,0 +1,43 @@
+'use strict'
+/*song description*/
+//song name
+var songname="Piano Man";
+//song author name
+var nameofauthor="William Martin Joel";
+//song lyricist
+var lyricist="billy joel";
+//guitarist
+var guitarist="William Martin Joel";
+//genre
+var genre="Pop";
+//artist
+var artist="Billy Joel";
+//albumname
+var albumname="Piano Man";
+//released year
+var Released=1973;
+//awards
+var awards="Grammy Hall of Fame";
+//time
+var durationInSecond=328;
+//song object
+var song={type:"Pop",artist:"Billy Joel",albumname:"Piano Man"};
+//song array["artist","author name","awards"]
+var songarray=["Billy Joel","William Martin Joel","Grammy Hall of Fame"];
+//Boolean expression
+var releaseddate="12121973";
+var booleanexpression=Boolean(releaseddate);//true
+/*console.log statements*/
+console.log(songname);
+console.log(nameofauthor);
+console.log(lyricist);
+console.log(guitarist);
+console.log(genre);
+console.log(artist);
+console.log(albumname);
+console.log(Released);
+console.log(awards);
+console.log(durationInSecond);
+console.log(song);
+console.log(songarray);
+console.log(booleanexpression);
